8ba0715ad90ca2dc50306b5fb94660a5e82114c3
BPBR <bernardo.palacio@gmail.com>
2023-05-26 18:39:26 +0000

    Add a cool input and math

--- first_Python.py
+++ first_Python.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
 #Hello World and good day
+
 print("Hello World!")
 print("Guten Tag!")
+num = input("Type a number to be added to 5: ")
+a=5+int(num)
+print("Your number plus 5 is " + str(a))
+
+
